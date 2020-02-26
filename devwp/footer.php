05e85cd47443e50dcb69526548ae41b66d7ebf7f
Josh Forrester <twicearoundmars@yahoo.com>
2020-02-26 13:36:22 -0500

    Switched the dymanic social links in the footer to static icons per client request.

--- devwp/footer.php
+++ devwp/footer.php
@@ -23,11 +23,21 @@
         <div class="col">
             <?php wp_nav_menu( array( 'theme_location' => 'footer-column-1' ) ); ?>
         </div>
+
         <div class="col">
             <?php wp_nav_menu( array( 'theme_location' => 'footer-column-2' ) ); ?>
         </div>
+
         <div class="col">
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-3' ) ); ?>
+<!--            Had to do it this way to the sections look the same-->
+            <ul>
+                <li class = "section-title"> <a>Let's Be Social</a></li>
+            </ul>
+            <div class="social-icons">
+                <a href="https://www.facebook.com/foothillscollective/"><i class="fab fa-facebook-square"></i></a>
+                <a href="https://www.instagram.com/foothills.collective/?hl=en"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/channel/UCX_IiZ45InnY5o85cB1i-BA"><i class="fab fa-youtube"></i></a>
+            </div>
         </div>
         <div class="clearfix"></div>
     </div>
