79f93d737d7d74031f08651bd5312f3d9ddc8b0f
Josh Forrester <twicearoundmars@yahoo.com>
2020-04-09 23:06:48 -0400

    Updated all the links on the resource page, will need to convert it to acf soon.

--- devwp/resource-album.php
+++ devwp/resource-album.php
@@ -35,12 +35,12 @@ get_header(); ?>
                     <hr>
 
                     <h4 class = "no-padding no-margin" >Where to Listen</h4>
-                    <p class = "no-padding no-margin"><a href="#" target="_blank"><i class="fab fa-spotify"></i> Listen On Spotify</a></p>
-                    <p class = "no-padding no-margin"><a href="#" target="_blank"><i class="fab fa-itunes"></i> Listen On Apple Music</a></p>
-                    <p class = "no-padding no-margin"><a href="#" target="_blank"><i class="fab fa-amazon"></i> Buy on Amazon</a></p>
-                    <p class = "no-padding no-margin"><a href="#" target="_blank"><i class="fab fa-youtube"></i> Watch on Youtube</a></p>
-                    <p class = "no-padding no-margin"><a href="#" target="_blank"><i class="fab fa-itunes-note"></i> Buy On iTunes</a></p>
-                    <p class = "no-padding no-margin"><a href="#" target="_blank"><i class="fab fa-google-play"></i> Listen on Google Play</a></p>
+                    <p class = "no-padding no-margin"><a href="https://open.spotify.com/album/6923qZBfY9Rl9FImYUBk1p" target="_blank"><i class="fab fa-spotify"></i> Listen On Spotify</a></p>
+                    <p class = "no-padding no-margin"><a href="https://music.apple.com/us/album/life-to-the-lion/1501389557?uo=4&app=music&at=1001lry3&ct=dashboard" target="_blank"><i class="fab fa-itunes"></i> Listen On Apple Music</a></p>
+                    <p class = "no-padding no-margin"><a href="https://www.amazon.com/Life-Lion-Foothills-Collective/dp/B085FPPZ1L/ref=sr_1_5?dchild=1&keywords=foothills+collective&qid=1586269690&s=dmusic&sr=1-5" target="_blank"><i class="fab fa-amazon"></i> Buy on Amazon</a></p>
+                    <p class = "no-padding no-margin"><a href="https://www.youtube.com/channel/UCX_IiZ45InnY5o85cB1i-BA" target="_blank"><i class="fab fa-youtube"></i> Watch on Youtube</a></p>
+                    <p class = "no-padding no-margin"><a href="https://iTunes.apple.com/us/album/life-to-the-lion/1501389557?uo=4&app=itunes&at=1001lry3&ct=dashboard" target="_blank"><i class="fab fa-itunes-note"></i> Buy On iTunes</a></p>
+                    <p class = "no-padding no-margin"><a href="https://play.google.com/store/music/album/Foothills_Collective_Life_to_the_Lion?id=Boxpuf6b2h5i6qo3fhfhpjssmmm" target="_blank"><i class="fab fa-google-play"></i> Listen on Google Play</a></p>
 
                     <h4 class = "no-padding no-margin margin-top-only">Release Year</h4>
                     <p class = "capitalize no-padding">2020</p>
@@ -60,19 +60,158 @@ get_header(); ?>
                         <h4 class = "uppercase padding-top-only-2">1 // Life to the lion</h4>
                     </span>
                     <span class = "float-right-soft padding-outer">
-                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="example-dropdown">Resources</button>
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-1">Resources</button>
                     </span>
-                    <div class="dropdown-pane" id="example-dropdown" data-dropdown data-auto-focus="true">
-                        <p class = "no-padding no-margin pink-link"><a href="#" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
-                        <p class = "no-padding no-margin"><a href="#" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                    <div class="dropdown-pane" id="resource-button-1" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Life-to-the-Lion-Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Life-to-the-Lion-Chord-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Life-to-the-Lion-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
+
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">2 // Revival Here</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-2">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-2" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/03/Revival-Here_Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/03/Revival-Here-Chord-Chart-1.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
                         <p class = "no-padding no-margin pink-link"><a href="#" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
                         <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
                     </div>
                     <hr class = "no-margin">
                 </div>
 
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">3 // I'm Forever Free</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-3">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-3" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Im-Forever-Free-Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Im-Forever-Free-Chord-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Im-Forever-Free-Number-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
+
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">4 // Forward</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-4">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-4" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Forward-Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Forward-Chord-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Forward-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
+
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">5 // Over and Over</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-5">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-5" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/03/Over-and-Over-Lyrics-1.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/03/Over-and-Over-Chord-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/03/Over-and-Over-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
+
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">6 // Love Runs Deep</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-6">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-6" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Love-Runs-Deep-Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Love-Runs-Deep-Chord-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Love-Runs-Deep-Number-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
+
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">7 // All Authority</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-7">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-7" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/All-Authority-Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/All-Authority-Chord-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/All-Authority-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
+
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">8 // Place of Peace (Spoken Word)</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-8">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-8" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Place-of-Peace-spoken-word-Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
+
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">9 // Place of Peace</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-9">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-9" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Place-of-Peace-Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Place-of-Peace-Chord-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Place-of-Peace-Number-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
 
 
+                <div class="small-12 cell">
+                    <span class = "float-left-soft">
+                        <h4 class = "uppercase padding-top-only-2">10 // Something New Something Now</h4>
+                    </span>
+                    <span class = "float-right-soft padding-outer">
+                        <button class="btn-hover color-1 no-margin" type="button" data-toggle="resource-button-10">Resources</button>
+                    </span>
+                    <div class="dropdown-pane" id="resource-button-10" data-dropdown data-auto-focus="true">
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Something-New-Something-Now-Lyrics.pdf" target="_blank"><i class="fas fa-download"></i> Lyric Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Something-New-Something-Now-Chord-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Chord Chart</a></p>
+                        <p class = "no-padding no-margin pink-link"><a href="https://foothillscollective.com/wp-content/uploads/2020/04/Something-New-Something-Now-Chart.pdf" target="_blank"><i class="fas fa-download"></i> Number Chart</a></p>
+                        <p class = "no-padding no-margin"><a href="https://www.multitracks.com/search/albums?albumID=3358" target="_blank"><i class="fas fa-external-link-alt"></i> Multitracks</a></p>
+                    </div>
+                    <hr class = "no-margin">
+                </div>
 
 
             </div>
