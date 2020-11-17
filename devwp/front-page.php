c03f063b985c7877ece209819e19efffa6a3d541
Myzwer <josh@onefortyfivedesign.com>
2020-11-17 15:23:45 -0500

    Made a few updates lol. Completely redesigned the homepage, still need to add ACF to make it dynamic.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -13,21 +13,146 @@
 
 get_header(); ?>
 
-    <video src="<?php the_field('video_background'); ?>" autoplay loop playsinline muted></video>
 
-    <div class="viewport-header">
-        <h1 class = "hero-header"><?php the_field('site_title'); ?></h1>
+    <div class="grid-container full-width">
+        <div class="grid-x grid-padding-x front-page-hero" style = "background: url(https://dalemyn.local/wp-content/uploads/2020/11/MTMTE-Banner.jpg);  background-position: center center;">
+            <div class="large-12 cell">
+                <div class="content-middle">
+                    <h1 class = "center light-color-invert">Foothills Collective</h1>
+                    <h2 class = "center light-color-invert">More Than Meets The Eye</h2>
+                    <div class="buttons">
+                        <a href = "#">
+                            <button class="btn-hover color-1-alt">Stream the EP Now!</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="full-width dark-background">
-        <div class = "grid-container full-width">
-            <div class="grid-x grid-padding-x">
-                <div class="small-12 large-10 large-offset-1 cell">
-                    <p class="featured-paragraph center"><?php the_field('featured_paragraph'); ?></p>
-                    <div class="buttons">
-                        <a href = "<?php the_field('button_link'); ?>">
-                            <button class="btn-hover color-1"><?php the_field('button_text'); ?></button>
-                        </a>
+
+    <!--DESKTOP-->
+    <div class="full-width main-background show-for-desktop">
+        <div class="grid-x">
+            <div class="medium-12 cell">
+                <div class="block">
+                    <div class="side-v2 side-v2--left">
+                        <div class="side-v2__bg"></div>
+                        <div class="side-v2__inner">
+                            <div class="side-v2__title">
+                                <h3 class = "dark-color-invert">"Truly" Is Available Everywhere!</h3>
+                                <div class="buttons">
+                                    <a href = "#">
+                                        <button class="btn-hover color-3">Stream the Single Now!</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="side-v2 side-v2--right">
+                        <div class="side-v2__bg background-2"></div>
+                        <div class="side-v2__inner">
+                            <div class="side-v2__title">
+                                <h3 class = "dark-color-invert">Check Out our BTS of Truly!</h3>
+                                <div class="buttons">
+                                    <a href = "#">
+                                        <button class="btn-hover color-1">Listen to the Podcast</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--END DESKTOP-->
+
+    <!--MOBILE-->
+    <div class="full-width main-background show-for-mobile">
+        <div class="grid-x">
+            <div class="small-12 cell">
+                <div class="slants">
+                    <div class="slant-top"></div>
+                    <h3 class = "dark-color-invert center add-padding">Stream "Truly" Wherever you listen to music!</h3>
+                    <div class="full-width">
+                        <div class="grid-x grid-padding-x grid-padding-y">
+                            <div class="small-6 cell">
+                                <a href="#">
+                                    <img class = "box-shadow" src="https://foothillscollective.com/wp-content/uploads/2020/10/Truly-Brand.jpg" alt="">
+                                    <h4 class = "dark-color-invert center">Song</h4>
+                                </a>
+                            </div>
+                            <div class="small-6 cell">
+                                <a href="#">
+                                    <img class = "box-shadow" src="https://foothillscollective.com/wp-content/uploads/2020/11/profile-image.jpg" alt="">
+                                    <h4 class = "dark-color-invert center">Podcast</h4>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--END MOBILE-->
+
+    <div class="full-width main-background">
+        <div class="grid-x">
+            <div class="small-12 cell">
+                <div class="image-banner-xlarge" style = "
+                background: linear-gradient(
+      rgba(0, 0, 0, 0.45),
+      rgba(0, 0, 0, 0.45)
+    ),url(https://dalemyn.local/wp-content/uploads/2020/03/59-_DSC5732-copy.jpg);
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: scroll;
+                background-size: cover;">
+                    <div class="grid-container">
+                        <div class="grid-x grid-padding-y">
+                            <div class="small-12 medium-offset-1 medium-5 relative cell">
+                                <img class = "box-shadow" src="https://foothillscollective.com/wp-content/uploads/2020/04/Life-to-the-Lion.jpg" alt="">
+                            </div>
+                            <div class="small-12 medium-6 relative cell">
+                                <div class="content-middle-medium add-padding">
+                                <h2 class = "text-overlay center"> Life to the Lion</h2>
+                                <h3 class = "text-overlay center">Stream Our Debut album, Live from Foothills Church</h3>
+                                    <hr>
+                                    <div class="buttons">
+                                        <a href = "#">
+                                            <button class="btn-hover color-1">Listen Now</button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width main-background">
+        <div class="grid-x">
+            <div class="small-12 cell">
+                <div class="image-banner-xlarge" style = "
+                background: linear-gradient(
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0)
+    ),url(https://foothillscollective.com/wp-content/uploads/2020/11/MTMTE-Background-White-Only.jpg);
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: scroll;
+                background-size: cover;">
+                    <div class="content-middle add-padding">
+                        <h2 class = "text-overlay light-color-invert center"> Get The Resources</h2>
+                        <h3 class = "text-overlay light-color-invert center">Tracks, Charts, Lyrics and More.</h3>
+                        <div class="buttons">
+                            <a href = "#">
+                                <button class="btn-hover color-1">Extend Your Setlist</button>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -37,54 +162,24 @@ get_header(); ?>
 
 
     <div class="full-width main-background">
-        <div class = "grid-container ">
-            <div class="grid-x grid-padding-x tb-padding">
-                <div class="large-12 cell">
-                    <a href="<?php the_field('top_featured_image_image_link'); ?>">
-                        <div class="image-banner rounded" style = "background: url(<?php the_field('top_featured_image_background_1'); ?>);background-position: center center;background-repeat: no-repeat;background-attachment: scroll;background-size: cover;">
-                            <div class = "content-middle">
-                                <h2 class = "text-overlay center">  <?php the_field('top_featured_image_text_line_1'); ?></h2>
-                                <h3 class = "text-overlay center"><?php the_field('top_featured_image_text_line_2'); ?></h3>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="small-12 medium-6 cell">
-                    <a href="<?php the_field('left_image_link_image_link'); ?>">
-                        <div class="image-banner rounded" style = "background: url(<?php the_field('left_image_link_background'); ?>);background-position: center center;background-repeat: no-repeat;background-attachment: scroll;background-size: cover;">
-                            <div class = "content-middle">
-                                <h2 class = "text-overlay uppercase center"><?php the_field('left_image_link_text_line_1'); ?></h2>
-                                <p class = "text-overlay-p center"><?php the_field('left_image_link_text_line_2'); ?></p>
-                                <div class="arrow bounce">
-                                    <div class="fa fa-arrow-down fa-2x"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="small-12 medium-6 cell">
-                    <a href="<?php the_field('right_image_link_image_link'); ?>">
-                        <div class="image-banner rounded" style = "background: url(<?php the_field('right_image_link_background'); ?>);background-position: center center;background-repeat: no-repeat;background-attachment: scroll;background-size: cover;">
-                            <div class = "content-middle">
-                                <h2 class = "text-overlay uppercase center"><?php the_field('right_image_link_text_line_1'); ?></h2>
-                                <p class = "text-overlay-p center"><?php the_field('right_image_link_text_line_2'); ?></p>
-                                <div class="arrow bounce">
-                                    <div class="fa fa-arrow-down fa-2x"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="small-12 cell">
-                    <div class="image-banner-large rounded" style = "background: url(<?php the_field('bottom_featured_image_background'); ?>);background-position: center center;background-repeat: no-repeat;background-attachment: scroll;background-size: cover;">
-                        <div class = "content-middle">
-                            <h2 class = "text-overlay center"><?php the_field('bottom_featured_image_text_line_1'); ?></h2>
-                            <div class="buttons center">
-                                <a href = "<?php the_field('bottom_featured_image_button_link'); ?>">
-                                    <button class="btn-hover color-1"><?php the_field('bottom_featured_image_button_text'); ?></button>
-                                </a>
-                            </div>
+        <div class="grid-x">
+            <div class="small-12 cell">
+                <div class="image-banner-xlarge" style = "
+                background: linear-gradient(
+      rgba(0, 0, 0, 0.45),
+      rgba(0, 0, 0, 0.45)
+    ),url(https://foothillscollective.com/wp-content/uploads/2020/02/IMG_0523-copy.jpg);
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: scroll;
+                background-size: cover;">
+                    <div class="content-middle add-padding">
+                        <h2 class = "text-overlay center">Follow Us On Youtube</h2>
+                        <h3 class = "text-overlay center">Music Vidoes, Worship Sets, BTS and More!</h3>
+                        <div class="buttons">
+                            <a href = "#">
+                                <button class="btn-hover color-1">Check us out!</button>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -93,5 +188,14 @@ get_header(); ?>
     </div>
 
 
+    <div class="full-width main-background padding-bottom">
+        <div class = "grid-container full-width">
+            <div class="grid-x grid-padding-x">
+                <div class="small-12 large-10 large-offset-1 cell">
+                    <p class="featured-paragraph"><?php the_field('featured_paragraph'); ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php get_footer();
